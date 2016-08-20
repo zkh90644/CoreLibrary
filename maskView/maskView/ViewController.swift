4a2c55db87fd5605e26467a11a3aa1258337c972
Creator <zkh90644@gmail.com>
2016-08-20 22:23:54 +0800

    修复了圆角内部的BUG，遗留角的位置的BUG

--- maskView/maskView/ViewController.swift
+++ maskView/maskView/ViewController.swift
@@ -45,7 +45,7 @@ class ViewController: UIViewController,UIGestureRecognizerDelegate {
         self.touchView = touchView
         
 //        获得被遮住后的图片的遮罩显示效果图
-        getImageWithMask()
+//        getImageWithMask()
     }
     
     func getImageWithMask() {
@@ -86,40 +86,108 @@ class ViewController: UIViewController,UIGestureRecognizerDelegate {
             (touchView?.frame.origin.y)! < maskText1.frame.origin.y + maskText1.frame.height &&
             (touchView?.frame.origin.x)! + (touchView?.frame.width)! >= maskText1.frame.origin.x &&
             touchView?.frame.origin.x < maskText1.frame.origin.x + maskText1.frame.width{
+           
             
-//            将产生的图片绘制到UIView的对应位置
-            let offsetX = maskText1.frame.origin.x - (touchView?.frame.origin.x)!
-            let offsetY = maskText1.frame.origin.y - (touchView?.frame.origin.y)!
             
-//            重新绘制UIImageView的layer
-            UIGraphicsBeginImageContextWithOptions((touchView?.frame.size)!, false, 2)
+//            //            将产生的图片绘制到UIView的对应位置
+//            let offsetX = maskText1.frame.origin.x - (touchView?.frame.origin.x)!
+//            let offsetY = maskText1.frame.origin.y - (touchView?.frame.origin.y)!
+//            
+//            //            重新绘制UIImageView的layer
+//            UIGraphicsBeginImageContextWithOptions((touchView?.frame.size)!, false, 2)
+//            
+//            //            获取上下文
+//            let context = UIGraphicsGetCurrentContext()
+//            
+//            //            获得原图片的layer
+//            backgroundImage!.drawAtPoint(CGPoint.init(x: 0, y: 0))
+//            
+//            //            绘制背景
+//            CGContextSetRGBFillColor(context, 1, 1, 1, 1)
+//            CGContextFillRect(context, CGRect.init(x: offsetX, y: offsetY, width: self.maskText1.frame.size.width, height: self.maskText1.frame.size.height))
+//            
+//            //            将渲染后的图放置到对应位置
+//            imageView.image?.drawInRect(CGRect.init(x: offsetX, y: offsetY, width: self.maskText1.frame.size.width, height: self.maskText1.frame.size.height))
+//            
+//            let tempImage = UIGraphicsGetImageFromCurrentImageContext()
+//            
+//            UIGraphicsEndImageContext()
+//            
+//            //            将对应的内容放置到touchView中
+//            touchView?.image = tempImage
+            changeImage()
             
-//            获取上下文
-            let context = UIGraphicsGetCurrentContext()
-
-//            获得原图片的layer
-            backgroundImage!.drawAtPoint(CGPoint.init(x: 0, y: 0))
-            
-//            绘制背景
-            CGContextSetRGBFillColor(context, 1, 1, 1, 1)
-            CGContextFillRect(context, CGRect.init(x: offsetX, y: offsetY, width: self.maskText1.frame.size.width, height: self.maskText1.frame.size.height))
-            
-//            将渲染后的图放置到对应位置
-            imageView.image?.drawInRect(CGRect.init(x: offsetX, y: offsetY, width: self.maskText1.frame.size.width, height: self.maskText1.frame.size.height))
-            
-            let tempImage = UIGraphicsGetImageFromCurrentImageContext()
-            
-            UIGraphicsEndImageContext()
-            
-//            将对应的内容放置到touchView中
-            touchView?.image = tempImage
         }else{
             touchView?.image = backgroundImage
         }
         
     }
     
+//    修改移动图片的Image
     
+    func changeImage(){
+        
+        
+//        创建对应图片在当前位置的修改图
+        let contentImage = getMaskImage()
+        
+//        绘制整个touchview
+        
+//            重新绘制UIImageView的layer
+        UIGraphicsBeginImageContextWithOptions((touchView?.frame.size)!, false, 2)
+        
+//            获取上下文
+        let context = UIGraphicsGetCurrentContext()
+        
+//        将界面用背景图先渲染一边
+        backgroundImage?.drawInRect(CGRect.init(x: 0, y: 0, width: (touchView?.frame.width)!, height: (touchView?.frame.height)!))
+        
+//        将内容图片绘制到对应的位置
+        let offsetX = maskText1.frame.origin.x - (touchView?.frame.origin.x)!
+        let offsetY = maskText1.frame.origin.y - (touchView?.frame.origin.y)!
+        
+        contentImage.drawInRect(CGRect.init(x: offsetX, y: offsetY, width: maskText1.frame.width, height: maskText2.frame.height))
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        
+//            关闭上下文
+        UIGraphicsEndImageContext()
+
+        touchView?.image = image
+        showImageView.image = image
+    }
+    
+    func getMaskImage() -> UIImage{
+        UIGraphicsBeginImageContextWithOptions((maskText1.frame.size), false, 2)
+        
+        let tempContext = UIGraphicsGetCurrentContext()
+        
+//        将产生的图片绘制到UIView的对应位置
+        let offsetX = (touchView?.frame.origin.x)! - maskText1.frame.origin.x
+        let offsetY = (touchView?.frame.origin.y)! - maskText1.frame.origin.y
+        
+//        先绘制背景
+        backgroundImage?.drawInRect(CGRect.init(x: offsetX, y: offsetY, width: (touchView?.frame.width)!, height: (touchView?.frame.height)!))
+        
+//        获得当前的图片
+        let backImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+//        绘制文字
+        let temp = UILabel.init(frame:maskText1.frame)
+        temp.font = maskText1.font
+        temp.text = maskText1.text
+        temp.textColor = UIColor.whiteColor()
+        
+        temp.layer.drawInContext(tempContext!)
+        
+        let currentImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        self.imageView.image = currentImage
+        
+        return currentImage
+    }
     
     
     func maskImage(image:UIImage, mask:(UIImage))->UIImage{
